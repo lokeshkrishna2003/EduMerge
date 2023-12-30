113ac5ce1b6e33dc5380cd77f987c0cb84beb13a
Lokesh Krishna Cherukuri <lokeshkrishnacherukuri@Lokeshs-MacBook-Air.local>
2023-12-30 23:35:30 +0530

    finalized loginandsignup styles and auth.jsx for version1

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-import Login from "./components/Login";
-import Signup from "./components/Signup";
+
+
+
 import VideoSubmission from "./components/VideoSubmission";
 import LandingPage from "./components/Landingpage/LandingPage";
+import AuthForm from "./components/AuthForm";
 
 const App = () => {
   useEffect(() => {
@@ -17,8 +19,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage/>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<AuthForm />} />
         <Route path="/submit-video" element={<VideoSubmission />} />
       </Routes>
     </Router>
