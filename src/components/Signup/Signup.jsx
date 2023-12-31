d1bd299b01e34c745b5bc427f7d448763d990cdb
Lokesh Krishna Cherukuri <lokeshkrishnacherukuri@Lokeshs-MacBook-Air.local>
2023-12-31 15:53:49 +0530

    displaying signup errors to the user

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -7,6 +7,7 @@ import { Link, useNavigate, } from 'react-router-dom';
 
 const Signup = () => {
   const [userData, setUserData] = useState({ username: '', email: '', password: '' });
+  const [error, setError] = useState(''); // New state for error messages
 
   const handleChange = (e) => {
     setUserData({ ...userData, [e.target.name]: e.target.value });
@@ -15,6 +16,7 @@ const Signup = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError(''); //Reset error message on new submission or Clears any existing error messages
     try {
       const response = await axios.post('http://localhost:3001/register', userData);
       console.log('successful signup', response.data);
@@ -22,14 +24,18 @@ const Signup = () => {
 
       // Additional success handling
     } catch (error) {
-      console.log('signup error', error);
-      // Additional error handling
+      if (error.response && error.response.data) {
+        setError(error.response.data); // Set the error message from response
+      } else {
+        setError('An error occurred during signup.'); // Fallback error message
+      }
     }
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-cover bg-center">
       <div className="bg-gradient-to-br from-gray-700 via-gray-800 to-gray-900 p-8 rounded-lg shadow-xl max-w-md mx-auto">
+
         <h2 className="text-3xl font-bold text-white mb-6 text-center">Sign Up for EduMerge Studio</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <input
@@ -65,10 +71,17 @@ const Signup = () => {
           >
             Sign Up
           </button>
+          
+
         </form>
+        <div className='w-full mt-3 h-4'>
+
+{error && <div className="text-red-500 mb-4 text-center" data-aos='zoom-in'>{error}</div>} {/* Error message display */}
+</div>
         <p className="mt-6 text-sm text-center text-gray-300">
           Already have an account? <Link to="/auth?mode=login" className="text-purple-400 hover:text-purple-300">Login</Link>
         </p>
+
       </div>
     </div>
   );
