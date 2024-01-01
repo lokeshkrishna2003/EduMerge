1767544360334ebb2b397ab625b847aa033e3fb9
Lokesh Krishna Cherukuri <lokeshkrishnacherukuri@Lokeshs-MacBook-Air.local>
2024-01-01 13:10:07 +0530

    refined the logic for scroll percentage visible only when there is a scroll length

--- src/components/ScrollProgress.jsx
+++ src/components/ScrollProgress.jsx
@@ -1,27 +1,42 @@
 // ScrollProgress.js
 import React, { useState, useEffect } from 'react';
-import AOS from 'aos';
+
 import 'aos/dist/aos.css';
 
 const ScrollProgress = () => {
   const [scrollPercent, setScrollPercent] = useState(0);
+  const [isScrollable, setIsScrollable] = useState(false);
 
   useEffect(() => {
-    AOS.init();
+    const checkIfScrollable = () => {
+      const scrollTotal = document.documentElement.scrollHeight - window.innerHeight;
+      setIsScrollable(scrollTotal > 0);
+    };
 
     const onScroll = () => {
+      if (!isScrollable) return; // Do nothing if the page is not scrollable
+
       const scrollTotal = document.documentElement.scrollHeight - window.innerHeight;
       const currentScroll = window.scrollY;
       setScrollPercent(Math.round((currentScroll / scrollTotal) * 100));
     };
 
+    // Check if the page is scrollable on initial load and window resize
+    checkIfScrollable();
+    window.addEventListener('resize', checkIfScrollable);
     window.addEventListener('scroll', onScroll);
 
-    return () => window.removeEventListener('scroll', onScroll);
-  }, []);
+    return () => {
+      window.removeEventListener('resize', checkIfScrollable);
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [isScrollable]);
+
+  if (!isScrollable) return null; // Don't render the component if the page is not scrollable
+
 
   return (
-    <div className="fixed top-[90%] left-[95%] z-20 -translate-x-1/2 -translate-y-1/2 w-20 h-20">
+    <div className="fixed bottom-5 right-5 z-20 w-20 h-20" data-aos="fade-left">
       <svg className="transform -rotate-90" width="80" height="80" viewBox="0 0 80 80">
         <circle className="text-gray-300" strokeWidth="5" stroke="currentColor" fill="transparent" r="35" cx="40" cy="40" />
         <circle className="text-violet-500" strokeWidth="5" strokeDasharray="219.91" strokeDashoffset={`${219.91 - (219.91 * scrollPercent) / 100}`} strokeLinecap="round" stroke="currentColor" fill="transparent" r="35" cx="40" cy="40" />
