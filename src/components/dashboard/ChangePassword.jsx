fe21427570c0b954bd28773f9563d6de9e11f936
Lokesh Krishna Cherukuri <lokeshkrishnacherukuri@Lokeshs-MacBook-Air.local>
2024-01-04 18:42:06 +0530

    modified the changepassword and delete account for better ui

--- src/components/dashboard/ChangePassword.jsx
+++ src/components/dashboard/ChangePassword.jsx
@@ -16,36 +16,36 @@ const ChangePassword = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 to-black" data-aos='zoom-out'>
-      <div className="p-10 rounded-lg shadow-xl max-w-lg mx-auto">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 to-black">
+      <div className="bg-gray-800 p-12 rounded-xl shadow-2xl max-w-2xl mx-auto">
         <h2 className="text-3xl font-bold text-white mb-8 text-center">Change Password</h2>
         <form onSubmit={handleSubmit} className="space-y-6">
           <div className="group relative border-b-2 border-gray-300 focus-within:border-purple-500">
-            <FaLock className="absolute left-0 top-3 text-gray-400 group-focus-within:text-purple-500" />
+            <FaLock className="absolute left-0 top-3 text-gray-400 group-focus-within:text-purple-500 text-xl" />
             <input
               type="password"
               name="oldPassword"
               value={passwordData.oldPassword}
               onChange={handleChange}
               placeholder="Old Password"
-              className="pl-10 pr-4 py-3 w-full bg-transparent border-none text-white focus:outline-none"
+              className="pl-10 pr-4 py-3 w-full bg-transparent border-none text-white focus:outline-none text-lg"
             />
           </div>
           <div className="group relative border-b-2 border-gray-300 focus-within:border-purple-500">
-            <FaLock className="absolute left-0 top-3 text-gray-400 group-focus-within:text-purple-500" />
+            <FaLock className="absolute left-0 top-3 text-gray-400 group-focus-within:text-purple-500 text-xl" />
             <input
               type="password"
               name="newPassword"
               value={passwordData.newPassword}
               onChange={handleChange}
               placeholder="New Password"
-              className="pl-10 pr-4 py-3 w-full bg-transparent border-none text-white focus:outline-none"
+              className="pl-10 pr-4 py-3 w-full bg-transparent border-none text-white focus:outline-none text-lg"
             />
           </div>
           {error && <div className="text-red-500 mt-4 mb-6 text-center">{error}</div>}
           <button
             type="submit"
-            className="w-full bg-purple-500 text-white px-4 py-3 rounded-md hover:bg-purple-600 transition duration-300"
+            className="bg-purple-500 text-white px-6 py-3 rounded-full hover:bg-purple-600 transition duration-300 flex items-center justify-center w-full text-lg"
           >
             Update Password
           </button>
