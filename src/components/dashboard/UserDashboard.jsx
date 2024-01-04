ae049ba715cc44daef8500beba124bb16afca409
Lokesh Krishna Cherukuri <lokeshkrishnacherukuri@Lokeshs-MacBook-Air.local>
2024-01-04 18:25:51 +0530

    modified userdashboard for greeting the user

--- src/components/dashboard/UserDashboard.jsx
+++ src/components/dashboard/UserDashboard.jsx
@@ -20,7 +20,7 @@ const UserDashboard = () => {
   const getGreeting = () => {
     const hour = new Date().getHours();
     if (hour < 12) return "Good Morning";
-    if (hour < 18) return "Good Afternoon";
+    if (hour < 15) return "Good Afternoon";
     return "Good Evening";
   };
 
