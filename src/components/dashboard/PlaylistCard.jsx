13e7fcfc6a41cf08b6291b9b7677b3982002ee8a
Lokesh Krishna Cherukuri <lokeshkrishnacherukuri@Lokeshs-MacBook-Air.local>
2024-01-12 00:18:58 +0530

    modified playlistcard  file by prettier

--- src/components/dashboard/PlaylistCard.jsx
+++ src/components/dashboard/PlaylistCard.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
-import { FiEdit, FiPlay ,FiTrash } from 'react-icons/fi';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { FiEdit, FiPlay, FiTrash } from "react-icons/fi";
+import { Link, useNavigate } from "react-router-dom";
 
-const PlaylistCard = ({ name, videoCount, onEdit ,onDelete ,onClickPlaylist }) => {
+const PlaylistCard = ({
+  name,
+  videoCount,
+  onEdit,
+  onDelete,
+  onClickPlaylist,
+}) => {
   const [hover, setHover] = useState(false);
   const navigate = useNavigate();
 
-  
-
   return (
     <div
-    data-aos='zoom-in'
+      data-aos="zoom-in"
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
       onClick={onClickPlaylist}
@@ -21,24 +25,28 @@ const PlaylistCard = ({ name, videoCount, onEdit ,onDelete ,onClickPlaylist }) =
           onClick={onEdit}
           className="text-white rounded-full p-1 hover:bg-gray-800 transition duration-300 ease-in-out transform hover:scale-110"
         >
-          <FiEdit className='text-purple-500 hover:text-white' size={15} />
+          <FiEdit className="text-purple-500 hover:text-white" size={15} />
         </button>
         <button
-              onClick={onDelete}
-              className="text-white rounded-full p-1 hover:bg-gray-800 transition duration-300 ease-in-out transform hover:scale-110"
-            >
-              <FiTrash className='text-purple-500  hover:text-white' size={15} />
-            </button>
+          onClick={onDelete}
+          className="text-white rounded-full p-1 hover:bg-gray-800 transition duration-300 ease-in-out transform hover:scale-110"
+        >
+          <FiTrash className="text-purple-500  hover:text-white" size={15} />
+        </button>
       </div>
 
       <div className="px-6 py-4">
         {hover ? (
-          <div className="flex justify-start items-center" >
-            <FiPlay  size={20} className="mr-2 mb-2 text-purple-500" data-aos='zoom-in' />
-            <div className="font-bold text-xl mb-2" >{name}</div>
+          <div className="flex justify-start items-center">
+            <FiPlay
+              size={20}
+              className="mr-2 mb-2 text-purple-500"
+              data-aos="zoom-in"
+            />
+            <div className="font-bold text-xl mb-2">{name}</div>
           </div>
         ) : (
-          <div className="font-bold text-xl mb-2" >{name}</div>
+          <div className="font-bold text-xl mb-2">{name}</div>
         )}
         <div className="text-sm text-gray-400">{videoCount} videos</div>
       </div>
