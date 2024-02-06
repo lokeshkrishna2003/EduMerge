a5a6cac29613e8515148ba181d7000de0e621d06
Lokesh Krishna Cherukuri <lci2022015@iiitl.ac.in>
2024-02-06 22:21:21 +0530

    modified the ui of login and signup page in mobile screens

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Login from "./login/Login";
 import Signup from "./Signup/Signup";
 import "./Signup/auth.css";
@@ -12,8 +12,8 @@ const AuthForm = () => {
   const isLogin = searchParams.get("mode") === "login";
 
   return (
-    <div className="flex bg-gradient-to-r from-gray-900 to-gray-700 min-h-screen">
-      <div className="w-1/2 relative overflow-hidden" data-aos="zoom-in">
+    <div className="flex bg-gradient-to-r from-gray-900 to-gray-700 min-h-screen md:flex-row">
+      <div className="hidden md:block md:w-1/2 relative overflow-hidden">
         <img
           src={isLogin ? heroImageLogin : heroImageSignup}
           className="image-animation w-full h-full rounded-[40vw]"
@@ -25,11 +25,17 @@ const AuthForm = () => {
           </p>
         </div>
       </div>
-      <div
-        className="w-1/2 bg-gradient-to-r from-gray-900 to-gray-700 flex items-center justify-center"
-        data-aos="fade-left"
-      >
-        {isLogin ? <Login /> : <Signup />}
+      <div className="md:hidden absolute inset-0 z-0">
+        <img
+          src={isLogin ? heroImageLogin : heroImageSignup}
+          className="w-full h-full object-cover opacity-50"
+          alt="Background"
+        />
+      </div>
+      <div className="md:w-1/2 w-full h-full flex items-center justify-center min-h-screen z-10">
+        <div className={`w-full ${isLogin ? 'px-5' : 'px-10'} py-8 md:py-0`}>
+          {isLogin ? <Login /> : <Signup />}
+        </div>
       </div>
     </div>
   );
